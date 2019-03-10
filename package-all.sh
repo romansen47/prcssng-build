0623d6410eb3ad74c8802cc67ae29e1407ea23dd
Ro Mansen <romansen47@gmail.com>
2019-03-10 18:46:59 +0000

    nothing special

--- package-all.sh
+++ package-all.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 mvn clean install 
-mvn javadoc:javadoc
+mvn sonar:sonar javadoc:javadoc
 
 sonar-scanner
 
 cd prcssng-temperature
 ant -f temperature.xml
-#mvn sonar:sonar
+mvn sonar:sonar
 cd ..
 
 cd prcssng-gameoflife
 ant -f gameoflife.xml
-#mvn sonar:sonar
+mvn sonar:sonar
 cd ..
 
 cd prcssng-chess
 ant -f chess.xml
-#mvn sonar:sonar
+mvn sonar:sonar
 cd ..
 
 cd prcssng-minesweeper
 ant -f minesweeper.xml
-#mvn sonar:sonar
+mvn sonar:sonar
 cd ..
 
 cd prcssng-Falcon
 ant -f falcon.xml
-#mvn sonar:sonar
+mvn sonar:sonar
 cp -r data target/
 cd ..
 
